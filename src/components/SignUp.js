5a7e808325dec44cdc4694f5050dab10d1fa9df4
Momin Mubash <108012519+momin-mubash@users.noreply.github.com>
2025-02-16 23:47:34 +0530

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
