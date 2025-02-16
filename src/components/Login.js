8826fb5a36e9b36670832467bac42e11bec032ef
Momin Mubash <108012519+momin-mubash@users.noreply.github.com>
2025-02-16 23:47:06 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,12 @@
+
 import React, { useState } from 'react';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
+// Login Component
 const Login = () => {
+  console.log("Login component rendered");
+  
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
@@ -13,9 +17,9 @@ const Login = () => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       navigate('/');
+      console.log("Successful login");
     } catch (error) {
-      console.error('Error logging in:', error);
-      // Handle error logging in (e.g., show error message to user)
+      console.log("Error in logging in:", error);
     }
   };
 
