d0ee0bc75e9cbaf28cd477b730323eb03ce8c55e
Momin Mubash <108012519+momin-mubash@users.noreply.github.com>
2025-02-16 23:39:09 +0530

    Create SignUp.js

---
+++ src/components/SignUp.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
+
+
+// SignUp Component
+const SignUp = () => {
+  console.log("SignUp component rendered");
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    const auth = getAuth();
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigate('/');
+      console.log("Successful sign-up");
+    } catch (error) {
+      console.log("Error in sign-up:", error);
+    }
+  };
+
+  return (
+    <div className="signup-container">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSignUp}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+export default SignUp;
